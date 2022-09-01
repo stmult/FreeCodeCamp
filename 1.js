c41e3b9b3670f8b7f4baee40af6fff8279d1439b
stmult <stmult@gmail.com>
2022-09-01 11:20:34 +0300

    2.0

---
+++ 1.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "I " + myVerb + " very " + myAdverb + " with my " + myAdjective + " cool " + myNoun; // Change this line
+// Only change code above this line
+console.log (wordBlanks);
